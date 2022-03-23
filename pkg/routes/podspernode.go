f488300b351bb802141c5e2617791848724b72d9
kunalp-gohire <kunalp.gohire@gmail.com>
2022-03-23 20:15:50 +0530

    change log format, BUILD_CONTAINER_IMAGE

--- pkg/routes/podspernode.go
+++ pkg/routes/podspernode.go
@@ -86,7 +86,7 @@ func (ppn *PodsPerNode) PodsPerNodeFilter(w http.ResponseWriter, r *http.Request
 	}
 
 	if response, err := json.Marshal(extenderFilterResult); err != nil {
-		ppn.log.Error(err.Error())
+		ppn.log.Error("Error in writing response", zap.String("Error", err.Error()))
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		errMsg := fmt.Sprintf("{'error':'%s'}", err.Error())
@@ -109,26 +109,24 @@ func (ppn *PodsPerNode) PodsPerNodeFilter(w http.ResponseWriter, r *http.Request
 
 // It checks if pod is of ReplicaSet set and returns nodes on which it can be scheduled.
 func (ppn *PodsPerNode) PodsPerNodeFilterHandler(args schedulerapi.ExtenderArgs) *schedulerapi.ExtenderFilterResult {
-
 	var canSchedule []corev1.Node
-	ppn.log.Info("Request for Pod", zap.String("PodName", args.Pod.Name))
 	podData, yes := ppn.isSchedulable(args.Pod)
 	if !yes {
 		result := schedulerapi.ExtenderFilterResult{
 			Nodes: args.Nodes,
 			Error: "",
 		}
-		ppn.log.Info("Skipping Pod, annotation not present or its owner type is not a replicaset")
+		ppn.log.Info("Skipping Pod, annotation not present or its owner type is not a replicaset", zap.String("PodName", args.Pod.Name))
 		return &result
 	}
 	var podNames []string
 	for _, node := range args.Nodes.Items {
 		msg, yes := ppn.canFit(node, args.Pod, podData)
 		if yes {
-			ppn.log.Info(args.Pod.Name, zap.String("can be schedule on node", node.Name))
+			ppn.log.Info("Can be schedule on node", zap.String("NodeName", node.Name), zap.String("PodName", args.Pod.Name))
 			val, err := ppn.ttlCache.Get(node.Name)
 			if err != nil && err != ttlcache.ErrNotFound {
-				ppn.log.Error(args.Pod.Name, zap.String("Error", err.Error()))
+				ppn.log.Error("Failed to get ttlCache", zap.String("PodName", args.Pod.Name), zap.String("Error", err.Error()))
 			}
 			if err == ttlcache.ErrNotFound {
 				podNames = append(podNames, args.Pod.Name)
@@ -138,13 +136,12 @@ func (ppn *PodsPerNode) PodsPerNodeFilterHandler(args schedulerapi.ExtenderArgs)
 			}
 			err = ppn.ttlCache.Set(node.Name, podNames)
 			if err != nil {
-				ppn.log.Error(args.Pod.Name, zap.String("Error", err.Error()))
+				ppn.log.Error("Failed to set ttl cache", zap.String("PodName", args.Pod.Name), zap.String("Error", err.Error()))
 			}
-			// ppn.log.Info(args.Pod.Name, zap.String("pod added in ttl", args.Pod.Name))
 			canSchedule = append(canSchedule, node)
 			break
 		} else {
-			ppn.log.Info(args.Pod.Name, zap.String("Cannot schedule on node", node.Name), zap.String("Reason", msg))
+			ppn.log.Info("Cannot schedule on node", zap.String("NodeName", node.Name), zap.String("PodName", args.Pod.Name), zap.String("Reason", msg))
 		}
 	}
 
@@ -183,21 +180,21 @@ func (ppn *PodsPerNode) isSchedulable(pod *corev1.Pod) (PodData, bool) {
 		}
 	}
 	if !isReplicaSet {
-		ppn.log.Info("Pod do not have owner type ReplicaSet")
+		ppn.log.Info("Pod do not have owner type ReplicaSet", zap.String("PodName", pod.Name))
 		return data, false
 	}
 
 	//get replicaset
 	replicaSet, err := ppn.replicaSetLister.ReplicaSets(pod.Namespace).Get(replicaSetName)
 	if err != nil {
-		ppn.log.Error(pod.Name, zap.String("Error", err.Error()))
+		ppn.log.Error("Failed to list replicaSet", zap.String("PodName", pod.Name), zap.String("Error", err.Error()))
 		return data, false
 	}
 
 	data.replicaSetName = replicaSetName
 	data.replica = *replicaSet.Spec.Replicas
 
-	ppn.log.Info(pod.Name, zap.String("ReplicaSetName", replicaSetName), zap.Int("MaxPods", data.maxPodsPerNode), zap.Int32("ReplicaCount", data.replica))
+	ppn.log.Info("Deployment data", zap.String("PodName", pod.Name), zap.String("ReplicaSetName", replicaSetName), zap.Int("MaxPods", data.maxPodsPerNode), zap.Int32("ReplicaCount", data.replica))
 	return data, true
 }
 
@@ -217,7 +214,7 @@ func (ppn *PodsPerNode) canFit(node corev1.Node, pod *corev1.Pod, podData PodDat
 
 	ttlPods, err := ppn.ttlCache.Get(node.Name)
 	if err != nil && err != ttlcache.ErrNotFound {
-		ppn.log.Error(pod.Name, zap.String("Error", err.Error()))
+		ppn.log.Error("ttlCache error", zap.String("PodName", pod.Name), zap.String("Error", err.Error()))
 	}
 
 	if ttlPods != nil {
@@ -235,7 +232,7 @@ func (ppn *PodsPerNode) canFit(node corev1.Node, pod *corev1.Pod, podData PodDat
 		}
 	}
 
-	ppn.log.Info(pod.Name, zap.String("ReplicaSetName", podData.replicaSetName), zap.String("NodeName", node.Name), zap.Int("PodCount", podCount), zap.Int32("ReplicaCount", podData.replica), zap.Int("MaxPods", podData.maxPodsPerNode))
+	ppn.log.Info("Scheduling data for pod", zap.String("PodName", pod.Name), zap.String("ReplicaSetName", podData.replicaSetName), zap.String("NodeName", node.Name), zap.Int("PodCount", podCount), zap.Int32("ReplicaCount", podData.replica), zap.Int("MaxPods", podData.maxPodsPerNode))
 
 	if podCount == 0 {
 		return "", true
